0b7ebc394cac0f339d70339d09cf17933a96d235
Julian Cole <juliancole@Julians-MacBook-Pro.local>
2020-11-11 16:15:10 -0800

    MVP

--- database/auth/auth-router.js
+++ database/auth/auth-router.js
@@ -1,22 +1,36 @@
 const router = require('express').Router();
+const Users = require("../models/user")
+const bcrypt = require("bcryptjs")
 const jwt = require("jsonwebtoken")
-const bcrypt = require('bcrypt')
-const
-router.post('/register', async (req, res, next) => {
+//current
+router.post("/register", async (req, res, next) => {
   try {
-    const newUser = await Users.add({
-    			username: req.body.username,
-    			// hash the password with a time complexity of "14"
-    			password: await bcrypt.hash(req.body.password, 14),
-    		})
-    		res.status(201).json(newUser)
+  		const { username, password } = req.body
+  		const user = await Users.findById({ username }).first()
+  		if (user) {
+  			return res.status(409).json({
+  				message: "Username is already taken",
+  			})
+  		}
+  		const newUser = await Users.add({
+  			username,
+  			// hash the password with a time complexity of "14"
+  			password: await bcrypt.hash(password, 14),
+  		})
+
+      const token = jwt.sign({
+  			userID: newUser.id,
+  		}, 'a-plus')
+  		// send the token back as a cookie
+  		req.session.token = token
+
+  		res.status(201).json(newUser)
   	} catch(err) {
   		next(err)
   	}
-});
+})
 
 router.post("/login", async (req, res, next) => {
-  console.log('test')
   try {
 		const { username, password } = req.body
 		const user = await Users.findBy({ username }).first()
@@ -36,17 +50,13 @@ router.post("/login", async (req, res, next) => {
 		}
 		// create a new session for the user
 		// req.session.user = user
+    //req.session.signedIn = true
     const token = jwt.sign({
 			userID: user.id,
 		}, 'a-plus')
-
 		// send the token back as a cookie
-		res.cookie("token", token)
-    console.log('token: ', token)
-		res.json({
-			message: `Welcome ${user.username}!`,
-		})
-    //var decoded = jwt.verify(token, 'shhhhh');
+		req.session.token = token
+
 		res.json({
 			message: `Welcome ${user.username}!`,
 		})
